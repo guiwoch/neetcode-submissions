03142a24429645345a7882861099288dc1856930
Guilherme Wochner <108285745+guiwoch@users.noreply.github.com>
2026-06-15 14:02:15 -0300

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <unordered_set>
+class Solution {
+  public:
+    int lengthOfLongestSubstring(std::string s) {
+        std::unordered_set<char> hash_set;
+        int left = 0;
+        int max = 0;
+        for (int right = 0; right < s.size(); ++right) {
+            while (hash_set.contains(s[right])) {
+                hash_set.erase(s[left]);
+                ++left;
+            }
+            hash_set.insert(s[right]);
+            max = std::max(max, (right - left + 1));
+        }
+        return max;
+    }
+};
