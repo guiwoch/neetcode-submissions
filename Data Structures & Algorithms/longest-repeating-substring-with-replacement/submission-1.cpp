bf53ac10bd924d0f2a0a7a43629b6bd198150b4d
Guilherme Wochner <108285745+guiwoch@users.noreply.github.com>
2026-06-16 15:46:24 -0300

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,22 @@
+// Longest Repeating Character Replacement
+#include <algorithm>
+#include <array>
+#include <string>
+class Solution {
+  public:
+    int characterReplacement(std::string s, int k) {
+        std::array<int, 26> count{};
+        int res = 0 ;
+        int left = 0;
+        for (int right = 0; right < s.size(); ++right) {
+            ++count[s[right] - 'A'];
+            while (right - left + 1 -
+                       *std::max_element(count.begin(), count.end()) > k) {
+                --count[s[left] - 'A'];
+                ++left;
+            }
+            res = std::max(res, right - left + 1);
+        }
+        return res;
+    }
+};
